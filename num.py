6d50d89181986bb3dd9992c34f2d8fe55a94833a
Kalpa18 <41140290+Kalpa18@users.noreply.github.com>
2018-07-12 12:53:46 +0530

    Create num.py

---
+++ num.py
@@ -0,0 +1,12 @@
+print("enter a number")
+N=int(input())
+if((N>=1)and(N<=100000)):
+  print("positive")
+else:
+  if(N==0):
+    print("zero")
+  elif(N<0):
+    print("negative")
+  else:
+    print("out of range")
+    
